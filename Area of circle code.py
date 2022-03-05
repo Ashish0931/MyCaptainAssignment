3a51d3a5620501613306ad87018b101b7104e83b
Ashish0931 <40832481+Ashish0931@users.noreply.github.com>
2022-03-05 19:22:28 +0530

    Add files via upload

---
+++ Area of circle code.py
@@ -0,0 +1,5 @@
+#this code prints area of circle
+radius = float(input("Enter the raidus of the circle: "))
+pi = 3.14159
+area= pi*(radius**2)
+print("Area of the circle with radius",radius, "is:",area)
