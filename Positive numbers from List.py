afb4e635f473b43ae8515ef8a24b3c40079de1fa
Ashish0931 <40832481+Ashish0931@users.noreply.github.com>
2022-03-12 15:04:36 +0530

    Add files via upload

---
+++ Positive numbers from List.py
@@ -0,0 +1,6 @@
+test_list=[23,-45,346,-2,-16,-473,-292,36,25] #test list for example 
+output_list=[]  #defining a list to show output 
+for i in test_list: #using loop to iterate through list 
+    if i>0:
+        output_list.append(i)   #Appening positive data into output list 
+print(output_list)
